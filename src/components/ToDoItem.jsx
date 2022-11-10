c968c8d8cc33057264f2986f2e276b92874fac35
AnushaPalle <anushapalle11@gmail.com>
2022-11-10 15:55:00 +0530

    Add CheckCircleIcon for todo task completion functionality

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import DeleteIcon from '@material-ui/icons/Delete';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import { useState } from "react";
 
 function ToDoItem(props) {
+  
+  const [taskCompleted, setTaskCompleted] = useState(false)
+
   return (
     <div>
-      <li>{props.text}
+      <li
+        style={{
+          textDecoration: taskCompleted ? "line-through": ""
+        }}
+      >
+        {props.text}
         <span className="icon">
+          <CheckCircleIcon
+            onClick={() => setTaskCompleted(prevTaskCompletedStatus => !prevTaskCompletedStatus)}
+          />
           <DeleteIcon
             onClick={() => {
             props.onChecked(props.id);
